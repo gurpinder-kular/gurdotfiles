d50882538f8e67851818469d5ae14330046cb8bf
gurpinder <gurpinder@tradebuilderinc.com>
2022-05-13 16:34:29 +0530

    fixes

--- softwares.sh
+++ softwares.sh
@@ -3,5 +3,5 @@ echo "##################################"
 echo "Installing softwares."
 
 sudo pacman -S flameshot mousepad evince gimp obs-studio mpv shotcut libreoffice-fresh bitwarden --noconfirm
-yay -S spotify --noconfirm
+yay -S spotify --answerdiff None --answerclean None
 
